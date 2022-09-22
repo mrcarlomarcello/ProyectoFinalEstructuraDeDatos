aaadde33c8323f7caa04546ba48150f66daee2db
dessire <dessireochoa12@gmail.com>
2022-09-22 17:29:33 -0600

    Menú Validado

--- main.cpp
+++ main.cpp
@@ -3,40 +3,138 @@ using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+bool EsUnNumero(const char* cadena)
+{
+  for( ; *cadena; ++cadena )
+  {
+    // En cuanto un caracter no sea numérico
+    if( !std::isdigit(*cadena) )
+      return false;
+  }
+
+  return true;
+}
+
 int main(int argc, char** argv) {
 	
-	int num;
+	int num = 0;
+	char menu[5];
 	int opcion;
+	bool continuar = true;
+	bool seguir = true;
 	
 	 do{
         cout << "\nMenú Principal"
              << "\n1. Algoritmos sobre Arboles"
              << "\n2. Algoritmos sobre Grafos"
              << "\n3. Salir" << endl;
-        cin >> num;
-        
-        if (num == 1){
-        	do{
-	        	cout << "\nAlgoritmos sobre Arboles"
-	             << "\n1. Codificador de Huffman"
-	             << "\n2. Decodificador de Huffman"
-	             << "\n3. Regresar al Menu Principal" << endl;
-	        	cin >> opcion;
-			}while (opcion < 3);
-        }
-        
-        if (num == 2){
-        	do{
-	        	cout << "\nAlgoritmos sobre Grafos"
-	             << "\n1. Leer grafo de un archivo"
-	             << "\n2. Prim"
-	             << "\n3. Floyd"
-	             << "\n4. Regresar al Menu Principal" << endl;
-	        	cin >> opcion;
-			}while (opcion < 4);
-        }
+        cin >> menu;
+
+		if (!EsUnNumero(menu) || (stoi(menu) > 3))
+			num = 4;
+		else
+			num = stoi(menu);
+			
+		switch (num){
+			case 1:
+			{
+				do{
+		        	cout << "\nAlgoritmos sobre Arboles"
+		             << "\n1. Codificador de Huffman"
+		             << "\n2. Decodificador de Huffman"
+		             << "\n3. Regresar al Menu Principal" << endl;
+		        	cin >> menu;
+	
+					if (!EsUnNumero(menu) || (stoi(menu) > 3))
+						num = 4;
+					else
+						num = stoi(menu);
+					
+					switch(num){
+						case 1:
+						{
+							//code
+							seguir = true;
+							break;	
+						}
+						case 2:
+						{
+							//code
+							seguir = true;
+							break;
+						}
+						case 3:
+						{
+							seguir = false;
+							break;
+						}
+						default:
+						{
+							cout << "\nEsa opcion es invalida" << endl;
+							seguir = true;
+							break;
+						}
+					}	
+				}while (seguir);
+				break;
+			}
+			case 2:
+			{
+				do{
+		        	cout << "\nAlgoritmos sobre Grafos"
+		             << "\n1. Leer grafo de un archivo"
+		             << "\n2. Prim"
+		             << "\n3. Floyd"
+		             << "\n4. Regresar al Menu Principal" << endl;
+		        	cin >> menu;
+	
+					if (!EsUnNumero(menu) || (stoi(menu) > 4))
+						num = 5;
+					else
+						num = stoi(menu);
+					
+					switch(num){
+						case 1:
+						{
+							//code
+							seguir = true;
+							break;	
+						}
+						case 2:
+						{
+							//code
+							seguir = true;
+							break;
+						}
+						case 3:
+						{
+							//code
+							seguir = true;
+							break;
+						}
+						case 4:
+						{
+							seguir = false;
+							break;
+						}
+						default:
+						{
+							cout << "\nEsa opcion es invalida" << endl;
+							seguir = true;
+							break;
+						}
+					}	
+				}while (seguir);
+				break;
+			}
+			case 3:
+				continuar = false;
+				break;
+			default:
+				cout << "\nEsa opcion es invalida" << endl;	
+		}
         	
-	}while (num < 3);
+	}while (continuar);
 	
 	return 0;
 }
